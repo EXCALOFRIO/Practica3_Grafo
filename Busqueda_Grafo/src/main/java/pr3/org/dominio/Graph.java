44869ae4761a43f042e617aae4e2d266444662e9
EXCALOFRIO <alejandro.ramirezlarena@usp.ceu.es>
2021-05-12 12:50:50 +0200

    V 4.5

--- Busqueda_Grafo/src/main/java/pr3/org/dominio/Graph.java
+++ Busqueda_Grafo/src/main/java/pr3/org/dominio/Graph.java
@@ -11,82 +11,101 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.Stack;
 
-public class Graph<V>{
-   
+public class Graph<V> {
+
     private Map<V, Set<V>> adjacenyList = new HashMap();
-    
-    public boolean addVertex(V v){
+
+    Stack st = new Stack();
+    ArrayList<V> path = new ArrayList<V>();
+
+    public boolean addVertex(V v) {
 
         if (adjacenyList.keySet().contains(v)) {
-            System.out.println("No se puede meter el vertice "+"{"+ v +"}"+ " porque ya esta en el grafo.");
+            System.out.println("No se puede meter el vertice " + "{" + v + "}" + " porque ya esta en el grafo.");
             return false;
-        }
-        else{
+        } else {
             adjacenyList.put(v, new HashSet());
             return true;
         }
     }
 
-    public boolean addEdge(V v1, V v2){
-        
+    public boolean addEdge(V v1, V v2) {
+
         Set<V> verticesConectados = adjacenyList.get(v1);
-        verticesConectados.add(v2);
+        if (verticesConectados.contains(v2)) {
+            System.out.println("La arista " + v1 + "-----" + v2 + " no se puede meter en el grafo porque ya existe.");
+            return false;
+
+        } else
+            verticesConectados.add(v2);
 
         verticesConectados = adjacenyList.get(v2);
         verticesConectados.add(v1);
         return true;
+
     }
 
-    public Set<V> obtainAdjacents(V v) throws Exception{
-       // System.out.println("Vertice adyacentes" + " del vertice " + v + " " + adjacenyList.get(v));
-        addArchivo("Vertice adyacentes" + adjacenyList.get(v));
-        System.out.println(toString());
-        return adjacenyList.get(v);
+    public Set<V> obtainAdjacents(V v) throws Exception {
+        // System.out.println("Vertice adyacentes" + " del vertice " + v + " " +
+        // adjacenyList.get(v));
+
+        if (adjacenyList.get(v) == null) {
+            throw new Exception("No existe el vértice");
+        } else {
+            addArchivo("Vertice adyacentes" + adjacenyList.get(v));
+            return adjacenyList.get(v);
+        }
+        // System.out.println(toString());
+
     }
 
-    public void addArchivo(String verticesAdycentes){
-        //AÑADIR VERTICE
+    public void addArchivo(String verticesAdycentes) {
+        // AÑADIR VERTICE
 
         try {
-			String NOMBRE_FICHERO = "Grafo.txt";
+            String NOMBRE_FICHERO = "Grafo.txt";
 
-			FileWriter fw=new FileWriter(NOMBRE_FICHERO);
-			BufferedWriter bw = new BufferedWriter(fw);
+            FileWriter fw = new FileWriter(NOMBRE_FICHERO);
+            BufferedWriter bw = new BufferedWriter(fw);
 
             bw.write(verticesAdycentes);
             bw.newLine();
             bw.close();
 
-
-		} catch (IOException e) {
-			System.out.println("NO SE HA PODIDO GENERAR EL GRAFO");
-			e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("NO SE HA PODIDO GENERAR EL GRAFO");
+            e.printStackTrace();
         }
-	
+
     }
 
-    public String toString(){
-       String datos = "";
-       String datos1 = "";
-       String datos2 = "";
-       for(V vertice : this.adjacenyList.keySet()){
-        datos1 = ("Vertice " +  adjacenyList.keySet()  + " metido en el grafo");
-        for(V adyacente : this.adjacenyList.get(vertice)){
-            datos2 = (" adyacentes" + " del vertice " + adyacente + " " + adjacenyList.get(adyacente));
-           }
-      }
-       datos = datos1 + datos2;
-       System.out.println(datos);
-       return datos;
+    public String toString() {
+        String datos = "";
+        for (V vertice : this.adjacenyList.keySet()) {
+            datos += (vertice.toString() + adjacenyList.get(vertice).toString() + "\n");
+        }
+        // System.out.println(datos);
+        return datos;
     }
 
-    public List<V> onePath(V v1, V v2) {
+    public List<V> onePath(V v1, V v2) throws Exception {
+        path.add(v1);
+
+        st.push(adjacenyList.get(v1));
+        while(st!=null){
+            if (String.valueOf(st.peek()).equals(v2)) {
+                
+                path.add((V) st.peek());
+                System.out.print("PATH"+path);
+            }else{
+                adjacenyList.get(st.peek());
+            }
+        }
+        System.out.print("PATHH de "+v1+" a "+ v2 +" "+path);
+
         return null;
     }
-    
-        
 
-				
-	
 }
--- Busqueda_Grafo/src/test/java/pr3/org/AppTest.java
+++ Busqueda_Grafo/src/test/java/pr3/org/AppTest.java
@@ -15,38 +15,30 @@ import pr3.org.dominio.Graph;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
 
     private void inicializarGrafo() {
         // añadir vertices
         // añadir aristas
-    
-    }
-    /*
-    @Before
-    public void inicializar(){
-        Graph<Integer> g = new Graph();
-        g.addVertex(3);
-        g.addVertex(9);
-        g.addVertex(4);
 
-        Graph<String> gs = new Graph();
-        gs.addVertex("a");
-        gs.addVertex("b");
     }
-    */
+
+    /*
+     * @Before public void inicializar(){ Graph<Integer> g = new Graph();
+     * g.addVertex(3); g.addVertex(9); g.addVertex(4);
+     * 
+     * Graph<String> gs = new Graph(); gs.addVertex("a"); gs.addVertex("b"); }
+     */
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 
     @Test
-    public void annadirVertice(){
+    public void annadirVertice() {
         System.out.println("\n\n------TEST------");
         System.out.println("-------1-------");
         Graph<Integer> g = new Graph();
@@ -56,37 +48,36 @@ public class AppTest
 
         Graph<String> gs = new Graph();
         assertTrue(gs.addVertex("a"));
-        String miTexto = "Vertice [a] metido en el grafo";
+        String miTexto = "a[]\n";
         gs.toString();
 
         System.out.println("Prueba to String");
         assertEquals(miTexto, gs.toString());
-        
-        
+
     }
 
     @Test
-    public void annadirEdge(){
+    public void annadirEdge() {
         System.out.println("\n\n------TEST------");
         System.out.println("-------2-------");
         Graph<Integer> g = new Graph();
         g.addVertex(3);
         g.addVertex(9);
         g.addVertex(4);
-        assertTrue(g.addEdge(3,9));
-        assertTrue(g.addEdge(3,4));
+        assertTrue(g.addEdge(3, 9));
+        assertTrue(g.addEdge(3, 4));
         g.toString();
 
         Graph<String> gs = new Graph();
         gs.addVertex("a");
         gs.addVertex("b");
-        assertTrue(gs.addEdge("a","b"));
-       
-        gs.toString();
+        assertTrue(gs.addEdge("a", "b"));
+
+        assertEquals("a[b]\nb[a]\n", gs.toString());
     }
 
-    @Test
-    public void obtainAdjacents() throws Exception{
+    @Test(expected = Exception.class)
+    public void obtainAdjacents() throws Exception {
         System.out.println("\n\n------TEST------");
         System.out.println("-------3-------");
         Graph<Integer> g = new Graph();
@@ -94,51 +85,45 @@ public class AppTest
         g.addVertex(9);
         g.addVertex(4);
         g.addVertex(7);
-        assertTrue(g.addEdge(3,9));
-        assertTrue(g.addEdge(4,7));
+        assertTrue(g.addEdge(3, 9));
+        assertTrue(g.addEdge(3, 7));
+        assertTrue(g.addEdge(4, 7));
         g.obtainAdjacents(3);
         g.obtainAdjacents(9);
         g.obtainAdjacents(4);
         g.obtainAdjacents(7);
-        g.toString();
+        g.onePath(3, 4);
+        System.out.println(g.toString());
 
         Graph<String> gs = new Graph();
         gs.addVertex("a");
         gs.addVertex("b");
         gs.addVertex("c");
         gs.addVertex("j");
-        assertTrue(gs.addEdge("a","b"));
-        assertTrue(gs.addEdge("a","c"));
-        assertTrue(gs.addEdge("j","c"));
-        assertTrue(gs.addEdge("c","b"));
-        assertTrue(gs.addEdge("c","c"));
+        assertTrue(gs.addEdge("a", "b"));
+        assertFalse(gs.addEdge("a", "b"));
+        assertTrue(gs.addEdge("a", "c"));
+        assertTrue(gs.addEdge("j", "c"));
+        assertTrue(gs.addEdge("c", "b"));
+        assertTrue(gs.addEdge("c", "c"));
         gs.obtainAdjacents("a");
         gs.obtainAdjacents("j");
         gs.obtainAdjacents("c");
-       
-        gs.toString();
+        gs.obtainAdjacents("op");
+        gs.onePath("a", "c");
+
+        System.out.println(gs.toString());
     }
 
     /*
-    @Test
-    public void onePathFindsAPath(){
-    System.out.println("\nTest onePathFindsAPath");
-    System.out.println("----------------------");
-    // Se construye el grafo.
-    Graph<Integer> g = new Graph<>();
-    g.addEdge(1, 2);
-    g.addEdge(3, 4);
-    g.addEdge(1, 5);
-    g.addEdge(5, 6);
-    g.addEdge(6, 4);
-    // Se construye el camino esperado.
-    List<Integer> expectedPath = new ArrayList<>();
-    expectedPath.add(1);
-    expectedPath.add(5);
-    expectedPath.add(6);
-    expectedPath.add(4);
-    //Se comprueba si el camino devuelto es igual al esperado.
-    assertEquals(expectedPath, g.onePath(1, 4));
-    }
-    */
+     * @Test public void onePathFindsAPath(){
+     * System.out.println("\nTest onePathFindsAPath");
+     * System.out.println("----------------------"); // Se construye el grafo.
+     * Graph<Integer> g = new Graph<>(); g.addEdge(1, 2); g.addEdge(3, 4);
+     * g.addEdge(1, 5); g.addEdge(5, 6); g.addEdge(6, 4); // Se construye el camino
+     * esperado. List<Integer> expectedPath = new ArrayList<>();
+     * expectedPath.add(1); expectedPath.add(5); expectedPath.add(6);
+     * expectedPath.add(4); //Se comprueba si el camino devuelto es igual al
+     * esperado. assertEquals(expectedPath, g.onePath(1, 4)); }
+     */
 }
